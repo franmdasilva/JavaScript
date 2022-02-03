9a505f16ac8695fc4eb6cb47688cb8a1daa3dba3
franmdasilva <franmdasilva@gmail.com>
2022-02-03 17:15:21 -0300

    03/02/22

---
+++ modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Fran', 99))
+console.log(mostraCidade('Fran', 'Floripa'))
+console.log(mostraHobby('Fran', 'assitir filmes'))
